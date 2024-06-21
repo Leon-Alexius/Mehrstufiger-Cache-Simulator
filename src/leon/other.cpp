a5132c6e608781612f34b66f6a13d108390aaf98
Dark-Rosaleen <Rosaleen@outlook.de>
2024-06-21 16:59:17 +0200

    test

---
+++ src/leon/other.cpp
@@ -0,0 +1,18 @@
+#include <systemc>
+
+int run_simulation(
+    int cycles, 
+    unsigned l1CacheLines, unsigned l2CacheLines, unsigned cacheLineSize, 
+    unsigned l1CacheLatency, unsigned l2CacheLatency, unsigned memoryLatency, 
+    size_t numRequests, struct Request requests,
+    const char* tracefile
+    ) {
+        return 0;
+    }
+
+// The default sc_main implementation.
+int sc_main(int argc, char* argv[])
+{
+    std::cout << "ERROR" << std::endl;
+    return 1;
+}
---
+++ src/leon/parser.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Assuming that the 'Result' and 'Request' structures and the 'run_simulation' function are defined in a header file named 'simulation.h'
+#include "simulation.h"
+
+extern int run_simulation(
+    int cycles, 
+    unsigned l1CacheLines, unsigned l2CacheLines, unsigned cacheLineSize, 
+    unsigned l1CacheLatency, unsigned l2CacheLatency, unsigned memoryLatency, 
+    size_t numRequests, struct Request requests,
+    const char* tracefile
+    );
+
+int main(int argc, char* argv[]) {
+    // check if arguments count is at least 10
+    if(argc < 11) {
+        printf("Insufficient arguments provided.\n");
+        return 1;
+    }
+
+    // parse inputs
+    int cycles = atoi(argv[1]);
+    unsigned l1CacheLines = atoi(argv[2]);
+    unsigned l2CacheLines = atoi(argv[3]);
+    unsigned cacheLineSize = atoi(argv[4]);
+    unsigned l1CacheLatency = atoi(argv[5]);
+    unsigned l2CacheLatency = atoi(argv[6]);
+    unsigned memoryLatency = atoi(argv[7]);
+    size_t numRequests = atoi(argv[8]);
+
+    struct Request requests[numRequests];
+
+    // Initialize 'requests' array here as needed
+    const char* tracefile = argv[9];
+
+    // run simulation
+    int result = 
+    run_simulation(
+        cycles, 
+        l1CacheLines, 
+        l2CacheLines, 
+        cacheLineSize, 
+        l1CacheLatency, 
+        l2CacheLatency, 
+        memoryLatency, 
+        numRequests, 
+        requests, 
+        tracefile
+    );
+
+    // Process the simulation result
+    if (result != 0) {
+        
+    }
+
+
+    return 0;
+}
