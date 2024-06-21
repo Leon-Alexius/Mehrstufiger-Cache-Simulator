7ccd35bbaea0d7fcbf2f3ed865811d48de3828c8
Dark-Rosaleen <Rosaleen@outlook.de>
2024-06-21 17:24:23 +0200

    parser and runner

---
+++ src/leon/placeHolder.hpp
@@ -0,0 +1,13 @@
+// temporary struct
+struct Request {
+    uint32_t addr;
+    uint32_t data;
+    int we;
+};
+
+struct Result {
+    size_t cycles;
+    size_t misses;
+    size_t hits;
+    size_t primitiveGateCount;
+};
