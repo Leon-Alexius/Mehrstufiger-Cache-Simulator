af8895778741dc8a4a0dc69312b736cb230f7464
Alexander Anthony Tang <go34dij@mytum.de>
2024-06-21 17:22:52 +0200

    Added ;

--- src/modules.hpp
+++ src/modules.hpp
@@ -5,11 +5,11 @@ struct Request {
     uint32_t addr;
     uint32_t data;
     int we;
-}
+};
 
 struct Result {
     size_t cycles;
     size_t misses;
     size_t hits;
     size_t primitiveGateCount
-}
+};
