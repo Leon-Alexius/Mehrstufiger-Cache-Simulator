936efb1f343ea50752244d4e3b4cbffe689372a3
nguv <go98pey@mytum.de>
2024-06-21 20:10:09 +0200

    unfinished CPU_L1_L1

--- src/modules/modules.hpp
+++ src/modules/modules.hpp
@@ -1,5 +1,6 @@
 // temporary struct
 #include<systemc>
+#include <cmath>
 struct Request {
     uint32_t addr;
     uint32_t data;
@@ -22,27 +23,44 @@ struct CPU_L1_L2 {
     unsigned memoryLatency;
     size_t numRequests;
     private unsigned cache_line_size;
-    private byte[][] data_blocks;
-    private byte[][] cache_blocks;
+    private char[][] data_blocks;
+    private char[][] cache_blocks;
   
     SC_MODULE(L1){
         sc_in<sc_bv<8*cache_line_size>> data_in;
-        sc_in<sc_bv<32>> address;
+        sc_in<sc_bv<32>> address_input;
         sc_in<bool> write_enable;
         sc_out<sc_bv<8*cache_line_size>> data_out;
         sc_bv <bool> hit;
+        sc_in<bool> clk;
 
         L1(sc_module_name name): sc_module(name){
-            
+            SC_THREAD(behaviour, clk.pos())
         }
 
         void behaviour(){
-            if (write_enable) {
+            while (true)
+            {
+                unsigned offset_bits_amount = (int)log2((float)cache_line_size);
+                unsigned index_bits_amount = (int) ceil((float)log2(64000/cache_line_size));
+                unsigned tag_bits_amount = 32 - offset_bits_amount - index_bits_amount;
+                unsigned address = (address_input->read()).to_uint();
+                unsigned address = address<<tag_bits_amount >>offset_bits_amount;
+
+                if (write_enable->read()) {
+                    data_block[][] = 
+                    data_out.write();
+                }else{
+                    
+                    data_out->write()
+                }
+
                 
-                data_out.write();
-            }else{
-                
+
+                wait();
             }
+            
+            
         }
     }
 
@@ -56,7 +74,7 @@ struct CPU_L1_L2 {
         sc_in<bool> clock; 
         sc_out<sc_bv<8*cacheLineSize>> data_out;
         
-        byte[][] memory_blocks;
+        char[][] memory_blocks;
         int latency;
         int count = 0;
         SC_CTOR(MEMORY);
@@ -82,7 +100,7 @@ struct CPU_L1_L2 {
                     bool buffer_out[cacheLineSize*8] = {};
                     for (int i = 0; i < cacheLineSize*8; i += 8) {
 
-                        byte result = memory_blocks[index1][index2];
+                        char result = memory_blocks[index1][index2];
                         // Convert the result into an array of bits
                         for (int j=0; j<8; j++) buffer_out[j + i] = ((theByte & (1<<(j))) != 0)      
 
