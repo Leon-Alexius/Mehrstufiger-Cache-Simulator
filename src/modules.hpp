74677dd6b11ba14b6173d07edad0135a7e8f4eb7
Alexander Anthony Tang <go34dij@mytum.de>
2024-06-21 17:09:12 +0200

    Added things for request and result

---
+++ src/modules.hpp
@@ -0,0 +1,15 @@
+/* anthony
+added Request and Result from the pdf.
+ */
+struct Request {
+    uint32_t addr;
+    uint32_t data;
+    int we;
+}
+
+struct Result {
+    size_t cycles;
+    size_t misses;
+    size_t hits;
+    size_t primitiveGateCount
+}
