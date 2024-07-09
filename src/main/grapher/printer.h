44c197d5d2217184fa56e60703d33ec1b624096a
Lie Leon Alexius <123194041+Dark-Rosaleen@users.noreply.github.com>
2024-07-09 05:54:22 +0200

    Update printer.h

--- src/main/grapher/printer.h
+++ src/main/grapher/printer.h
@@ -1,7 +1,7 @@
 #ifndef PRINTER_H
 #define PRINTER_H
 
-void print_layout();
+void print_layout(int cycles, int l1CacheLines, int l2CacheLines, int cacheLineSize, int l1CacheLatency, int l2CacheLatency, int memoryLatency, int numRequests);
 int load_image(const char *filename);
 
 // Test Case
