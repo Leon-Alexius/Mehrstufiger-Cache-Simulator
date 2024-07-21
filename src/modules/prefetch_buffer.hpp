57558bb7242a91f7915d9c9976df97ba7afcc3bc
Trang <nguyenvantrangvt@gmail.com>
2024-07-21 19:13:00 +0200

    clean up

--- src/modules/prefetch_buffer.hpp
+++ src/modules/prefetch_buffer.hpp
@@ -16,19 +16,27 @@ SC_MODULE(PREFETCH){
 
     sc_fifo<char*> prefetch;
     sc_fifo<uint32_t> address_prefetch;
-    unsigned head = 0;
-    unsigned tail = 0;
-    unsigned capacity;
-
-    bool empty = true;
-
-   
+    unsigned capacity,
+    
     SC_CTOR(PREFETCH);
-    PREFETCH(sc_module_name name, unsigned capacity) : sc_module(name), capacity(capacity), prefetch(capacity), address_prefetch(capacity) {
+    PREFETCH(sc_module_name name, unsigned capacity) : sc_module(name), capacity(capacity) prefetch(capacity), address_prefetch(capacity) {
 
     };
 
-   
+
+    /**
+     * @brief A write method from the buffer.
+     * 
+     * @param data A variable of a pointer which consists of an array of length cacheLineSize to be read from.
+     * @param address A variable of the address corresponding to the data
+     * 
+     * @return Returns true if a read is successful, false if the buffer is empty.
+     * If the buffer is empty then it does not change the state of the FIFO.
+     *
+     * @authors
+     * Alexander Anthony Tang
+     * Van Trang Nguyen
+    */
     bool write(char* data, uint32_t address) {
         wait(SC_ZERO_TIME);
         wait(SC_ZERO_TIME);
@@ -44,11 +52,15 @@ SC_MODULE(PREFETCH){
     /**
     * @brief A read method from the buffer.
     * 
-    * @param data A variable of a pointer which consists of an array of length 4 to be written to.
+    * @param data A variable of a pointer which consists of an array of length cacheLineSize to be written to.
     * @param address A variable of the address to be written to
     * 
     * @return Returns true if a read is successful, false if the buffer is empty.
     * If the buffer is empty then it does not change the state of the FIFO.
+    *
+    * @authors
+    * Alexander Anthony Tang
+    * Van Trang Nguyen
     */
     bool read(char*& data, uint32_t& address) {
         wait(SC_ZERO_TIME);
@@ -57,23 +69,12 @@ SC_MODULE(PREFETCH){
         
         if (data_avail && address_avail) {
             wait(SC_ZERO_TIME);
-            head = (head + 1) % capacity;
-            if (head == tail) empty = true;
             return true;
         } else {
             return false;
         }
     }
 
-
-    /**
-    * @brief This method checks if the buffer is empty
-    * 
-    * @return Returns true if it is empty, false if not.
-    */
-    bool is_empty() {
-        return empty;
-    }
 };
 
 #endif
