ff36208b73f609879bf490e7bfa34ef373ecba4b
Trang <nguyenvantrangvt@gmail.com>
2024-07-21 19:18:20 +0200

    bbru

--- src/modules/prefetch_buffer.hpp
+++ src/modules/prefetch_buffer.hpp
@@ -12,14 +12,23 @@ using namespace sc_core;
 using namespace std;
 
 
+/**
+* @brief The prefetch buffer tries to increase read hits to reduce latency 
+* @details This module will read from the memory and will wait according to the memory latency, and therefore making it
+* slower.
+* 
+* @authors
+* Alexander Anthony Tang
+* Van Trang Nguyen
+*/
 SC_MODULE(PREFETCH){
 
     sc_fifo<char*> prefetch;
     sc_fifo<uint32_t> address_prefetch;
-    unsigned capacity,
+    unsigned capacity;
     
     SC_CTOR(PREFETCH);
-    PREFETCH(sc_module_name name, unsigned capacity) : sc_module(name), capacity(capacity) prefetch(capacity), address_prefetch(capacity) {
+    PREFETCH(sc_module_name name, unsigned capacity) : sc_module(name), capacity(capacity), prefetch(capacity), address_prefetch(capacity) {
 
     };
 
@@ -42,7 +51,6 @@ SC_MODULE(PREFETCH){
         wait(SC_ZERO_TIME);
         
         if (prefetch.nb_write(data) && address_prefetch.nb_write(address)) {
-            empty = false;
             return true;
         } else {
             return false;
